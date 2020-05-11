9a67dc6200e79e849def2df7c5f5e384ac859f7c
Brianna <briannakeune@gmail.com>
2020-05-11 10:58:04 -0500

    init project setup

---
+++ tk_challenges/linked_lists.py
@@ -0,0 +1,5 @@
+# Draw/model out inserting a new element into a linked list with only one element.
+
+# Draw/model out how to traverse through a linked list in order to find a target value.
+
+# Draw/model out how to traverse through a linked list in order to find the maximum value in the linked list. 
