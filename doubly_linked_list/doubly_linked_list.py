c24e7b29750235e6ec62961fdfd604ba9c87eb18
Brianna <briannakeune@gmail.com>
2020-05-13 10:56:15 -0500

    still having a length issue with move_to functions

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -4,18 +4,13 @@ as well as its next node in the List."""
 
 class ListNode:
     def __init__(self, value, prev=None, next=None):
-        self.value = value  # current node in list
-        self.prev = prev  # stores, previous node
-        self.next = next  # stores next node
+        self.value = value  # the node currently being pointed at's value
+        self.prev = prev  # stores pointer to find prev node
+        self.next = next  # stores pointer to find next node
 
     """Wrap the given value in a ListNode and insert it
     after this node. Note that this node could already
     have a next node it is pointing to."""
-
-    """
-    a_saved_node = ListNode(1)
-    a_saved_node.insert_after(2)
-    """
 
     def insert_after(self, value):
         current_next = self.next
@@ -108,15 +103,22 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly."""
 
     def add_to_tail(self, value):
-        # saving prev_tail to handle pointer
-        prev_tail = self.tail
-        # wraps the given value in a listnode, and adds self.tail to prev spot
-        new_tail = ListNode(value, self.tail)
-        # inserts value as new tail of the list
-        self.tail = new_tail
-        # handles prev_tails next pointer
-        # to point to new tail
-        prev_tail.next = self.tail
+        listed_node = ListNode(value)
+        # what if self.tail is None
+        if self.head is None and self.tail is None:
+            # I should figure out how to make this where we have to add to the head?
+            # or does that not matter because dll can be circular.
+            self.head, self.tail = listed_node, listed_node
+        # what if it isn't
+        else:
+            prev_tail = self.tail
+            # wraps the given value in a listnode, and adds self.tail to prev spot
+            new_tail = ListNode(value, self.tail)
+            # inserts value as new tail of the list
+            self.tail = new_tail
+            # handles prev_tails next pointer
+            # to point to new tail
+            prev_tail.next = self.tail
         self.length += 1
 
     """Removes the List's current tail node, making the
@@ -148,8 +150,15 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
-        if node is self.head:
+        if node is self.head and len(self) is 1:
             return None
+        elif node is self.tail:
+            # if we take the value out the add to head function works
+            node_to_move = node.value
+            # setting new tail to be the prev_tail's prev
+            self.tail = node.prev
+            self.add_to_head(node_to_move)
+            print('self.head in mtf elif: ', self.head)
         else:
             # holding moving nodes data
             node_to_move = node
@@ -157,21 +166,30 @@ class DoublyLinkedList:
             node_to_move.prev.next, node_to_move.next.prev = node_to_move.next, node_to_move.prev
 
             # making node to move the new head of the list
-            node.add_to_head(node_to_move)
+            self.add_to_head(node_to_move)
+            print('self.head in mtf else: ', self.head)
 
     """Removes the input node from its current spot in the
     List and inserts it as the new tail node of the List."""
 
     def move_to_end(self, node):
-        if node is self.tail:
+        if node is self.tail and len(self) is 1:
             return None
+        elif node is self.head:
+            # if we take the value out the add_to_tail function works
+            node_to_move = node.value
+            # setting new head to be the prev head's next
+            self.head = node.next
+            self.add_to_tail(node_to_move)
+            print('self.tail in mtt elif: ', self.tail)
         else:
             # holding moving node's data
             node_to_move = node
             # making a link between node's prev, next attrs
             node_to_move.prev.next, node_to_move.next.prev = node_to_move.next, node_to_move.prev
             # making node to move the new tail of the list
-            node.add_to_tail(node_to_move)
+            self.add_to_tail(node_to_move)
+            print('self.tail in mtt else: ', self.tail)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
@@ -197,11 +215,13 @@ class DoublyLinkedList:
             sorted_list = sorted([i.value for i in self])
             return sorted_list[-1]
 
+    # creates a way to make this list iterable for get_max function
     def __iter__(self):
         current = self.head
         while current is not None:
             yield current
             current = current.next
 
+    # assists in printing values for returns
     def __str__(self):
         return str(self)
